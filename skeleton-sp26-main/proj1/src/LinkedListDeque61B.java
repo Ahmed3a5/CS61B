05e6f58ce3fb79833bfb2e888b61d5d8b1c192ba
Ahmed3a5 <ahmefhany343@gmail.com>
2026-08-25 20:00:34 +0300

    finish the project 1 add all methods and tests

--- skeleton-sp26-main/proj1/src/LinkedListDeque61B.java
+++ skeleton-sp26-main/proj1/src/LinkedListDeque61B.java
@@ -1,10 +1,17 @@
-// package skeleton-sp26-main.proj1.src;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class LinkedListDeque61B<T> implements Deque61B<T> {
 
     private class Node<T>{
+
+
+        /*
+            make the private class node whic is the small unit that make the linked list 
+            it have a previous and next pointers to the other nodes 
+            and the item whic is the value the node have 
+         */
         Node prev;
         T item;
         Node next;
@@ -17,85 +24,158 @@ public class LinkedListDeque61B<T> implements Deque61B<T> {
         }
     }
 
+    /*
+        make the sentinel node variable and size variable 
+     */
     private Node sentinel;
     private int size;
 
     public LinkedListDeque61B(){
+        // intialize the sentinel Node with all null //
 
         sentinel = new Node(null , null , null);
+
+        // make the sentinel node point to itself
+
         sentinel.prev = sentinel;
         sentinel.next = sentinel;
+
+        // intialize the size of the list as 0 empty list 
+
         size =0;
 
     }
 
     @Override
     public void addFirst(T x) {
-        Node n = new Node(sentinel , x , sentinel.next);
-        sentinel.prev.prev = n;
+        /*
+            intialize the new node have the x value to add 
+            the new node previous point to the sentinel and the next point to the next node to the sentinel 
+            then the next node to sentinel prev pointer point to the n node 
+            then the sentinel next point to the new node 
+            then increase the size by one 
+        */
+        Node n = new Node(null , x , null);
+        n.prev = sentinel;
+        n.next = sentinel.next;
+        sentinel.next.prev = n;
         sentinel.next = n;
         size++;
     }
 
     @Override
     public void addLast(T x) {
-        Node n = new Node(sentinel.prev ,x , sentinel);
+        /*
+            intialize the new node to add 
+            make its prev point to the last node whic is sentinle prev point 
+            then make the next point of the new node point to the sentinel circular 
+            make the last node in the list next pointer point to the new last node 
+            increase the size by one 
+
+        */
+        Node n = new Node(null,x , null);
+        n.prev = sentinel.prev;
+        n.next = sentinel;
         sentinel.prev.next = n;
+        sentinel.prev = n;
         size++;
     }
 
     @Override
     public List<T> toList() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toList'");
+        List returnlist = new ArrayList<>();
+        if(this.isEmpty()){
+            return returnlist;
+        }
+
+        Node p = sentinel.next;
+
+        while(p!=sentinel){
+            returnlist.add(p.item);
+            p = p.next;
+        }
+        return returnlist;
     }
 
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+       if(size == 0){
+        return true;
+       }
+       return false;
     }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
+       return this.size;
     }
 
     @Override
     public T getFirst() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getFirst'");
+        return (T) sentinel.next.item;
     }
 
     @Override
     public T getLast() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getLast'");
+        return (T) sentinel.prev.item;
     }
 
     @Override
     public T removeFirst() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeFirst'");
+        T removeditem = (T) sentinel.next.item;
+        sentinel.next.next.prev = sentinel;
+        sentinel.next = sentinel.next.next;
+        size--;
+        
+        return removeditem;
     }
 
     @Override
     public T removeLast() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeLast'");
+        T removeditem = (T) sentinel.prev.item;
+        sentinel.prev.next = sentinel;
+        sentinel.prev = sentinel.prev.prev;
+        size--;
+
+        return removeditem;
     }
 
     @Override
     public T get(int index) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
+        if(index > size){
+            return null;
+        }
+       
+        Node p = sentinel.next;
+        
+        for(int i = 0 ; i < size ; i++){
+            if(i == index){
+                return (T) p.item;
+            }
+
+            p = p.next;
+        }
+        return null;
+    }
+
+    private T getRecursive( Node p , int index, int i){
+        if(i == index){
+            return (T) p.item;
+        }
+        else{
+            return getRecursive(p.next, index, i+1);
+        }
     }
 
     @Override
     public T getRecursive(int index) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRecursive'");
+        if(index > size){
+            return null;
+        }
+        int i = 0;
+        Node p = sentinel.next;
+        T returneditem = getRecursive(p, index, i);
+        return returneditem;
     }
     
 
--- skeleton-sp26-main/proj1/tests/LinkedListDeque61BTest.java
+++ skeleton-sp26-main/proj1/tests/LinkedListDeque61BTest.java
@@ -8,53 +8,185 @@ import static com.google.common.truth.Truth.assertWithMessage;
 /** Performs some basic linked list tests. */
 public class LinkedListDeque61BTest {
 
-    // @Test
-    // /** In this test, we have three different assert statements that verify that addFirst works correctly. */
-    // public void addFirstTestBasic() {
-    //     Deque61B<String> lld1 = new LinkedListDeque61B<>();
+    @Test
+    /** In this test, we have three different assert statements that verify that addFirst works correctly. */
+    public void addFirstTestBasic() {
+        Deque61B<String> lld1 = new LinkedListDeque61B<>();
 
-    //     lld1.addFirst("back"); // after this call we expect: ["back"]
-    //     assertThat(lld1.toList()).containsExactly("back").inOrder();
+        lld1.addFirst("back"); // after this call we expect: ["back"]
+        assertThat(lld1.toList()).containsExactly("back").inOrder();
 
-    //     lld1.addFirst("middle"); // after this call we expect: ["middle", "back"]
-    //     assertThat(lld1.toList()).containsExactly("middle", "back").inOrder();
+        lld1.addFirst("middle"); // after this call we expect: ["middle", "back"]
+        assertThat(lld1.toList()).containsExactly("middle", "back").inOrder();
 
-    //     lld1.addFirst("front"); // after this call we expect: ["front", "middle", "back"]
-    //     assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
+        lld1.addFirst("front"); // after this call we expect: ["front", "middle", "back"]
+        assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
 
-    //     /* Note: The first two assertThat statements aren't really necessary. For example, it's hard
-    //        to imagine a bug in your code that would lead to ["front"] and ["front", "middle"] failing,
-    //        but not ["front", "middle", "back"].
-    //      */
-    // }
+        /* Note: The first two assertThat statements aren't really necessary. For example, it's hard
+           to imagine a bug in your code that would lead to ["front"] and ["front", "middle"] failing,
+           but not ["front", "middle", "back"].
+         */
+    }
 
-    // @Test
-    // /** In this test, we use only one assertThat statement. IMO this test is just as good as addFirstTestBasic.
-    //  *  In other words, the tedious work of adding the extra assertThat statements isn't worth it. */
-    // public void addLastTestBasic() {
-    //     Deque61B<String> lld1 = new LinkedListDeque61B<>();
+    @Test
+    /** In this test, we use only one assertThat statement. IMO this test is just as good as addFirstTestBasic.
+     *  In other words, the tedious work of adding the extra assertThat statements isn't worth it. */
+    public void addLastTestBasic() {
+        Deque61B<String> lld1 = new LinkedListDeque61B<>();
 
-    //     lld1.addLast("front"); // after this call we expect: ["front"]
-    //     lld1.addLast("middle"); // after this call we expect: ["front", "middle"]
-    //     lld1.addLast("back"); // after this call we expect: ["front", "middle", "back"]
-    //     assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
-    // }
+        lld1.addLast("front"); // after this call we expect: ["front"]
+        lld1.addLast("middle"); // after this call we expect: ["front", "middle"]
+        lld1.addLast("back"); // after this call we expect: ["front", "middle", "back"]
+        assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
+    }
 
-    // @Test
-    // /** This test performs interspersed addFirst and addLast calls. */
-//     public void addFirstAndAddLastTest() {
-//         Deque61B<Integer> lld1 = new LinkedListDeque61B<>();
+    @Test
+    /** This test performs interspersed addFirst and addLast calls. */
+    public void addFirstAndAddLastTest() {
+        Deque61B<Integer> lld1 = new LinkedListDeque61B<>();
 
-//         /* I've decided to add in comments the state after each call for the convenience of the
-//            person reading this test. Some programmers might consider this excessively verbose. */
-//         lld1.addLast(0);   // [0]
-//         lld1.addLast(1);   // [0, 1]
-//         lld1.addFirst(-1); // [-1, 0, 1]
-//         lld1.addLast(2);   // [-1, 0, 1, 2]
-//         lld1.addFirst(-2); // [-2, -1, 0, 1, 2]
+        /* I've decided to add in comments the state after each call for the convenience of the
+           person reading this test. Some programmers might consider this excessively verbose. */
+        lld1.addLast(0);   // [0]
+        lld1.addLast(1);   // [0, 1]
+        lld1.addFirst(-1); // [-1, 0, 1]
+        lld1.addLast(2);   // [-1, 0, 1, 2]
+        lld1.addFirst(-2); // [-2, -1, 0, 1, 2]
 
-//         assertThat(lld1.toList()).containsExactly(-2, -1, 0, 1, 2).inOrder();
-//     // }
+        assertThat(lld1.toList()).containsExactly(-2, -1, 0, 1, 2).inOrder();
+    }
 
-//     // Below, you'll write your own tests for LinkedListDeque61B.
-// }
+    // Below, you'll write your own tests for LinkedListDeque61B.
+
+    @Test 
+    
+    // test the is empty method 
+
+    public void testempty(){
+        Deque61B<Integer> LLd = new LinkedListDeque61B<>();
+
+        assertThat(LLd.isEmpty()).isEqualTo(true);
+
+        LLd.addFirst(0);
+        LLd.addFirst(4);
+        LLd.addLast(7);
+
+        assertThat(LLd.isEmpty()).isEqualTo(false);
+    }
+
+    @Test
+    // test the size method 
+
+    public void testsize(){
+        Deque61B<Integer> LLd = new LinkedListDeque61B<>();
+
+        assertThat(LLd.size()).isEqualTo(0);
+
+
+        LLd.addFirst(0);
+        LLd.addFirst(4);
+        LLd.addLast(7);
+
+        assertThat(LLd.size()).isEqualTo(3);
+    }
+
+
+    @Test
+
+    // test the getfirst method 
+
+    public void testgetfirst(){
+
+        Deque61B<Integer> LLd = new LinkedListDeque61B<>();
+        LLd.addFirst(0);
+        LLd.addFirst(4);
+        LLd.addLast(7);
+
+        assertThat(LLd.getFirst()).isEqualTo(4);
+    }
+
+
+     @Test
+
+    // test the getlast method 
+
+    public void testgetlast(){
+
+        Deque61B<Integer> LLd = new LinkedListDeque61B<>();
+        LLd.addFirst(0);
+        LLd.addFirst(4);
+        LLd.addLast(7);
+        LLd.addLast(8);
+
+
+        assertThat(LLd.getLast()).isEqualTo(8);
+    }
+
+    @Test
+    // test the remove last method 
+
+    public void testremovelast(){
+        Deque61B<Integer> LLd = new LinkedListDeque61B<>();
+        LLd.addFirst(0);
+        LLd.addFirst(4);
+        LLd.addLast(7);
+        LLd.addLast(8);
+        int removeditem  = LLd.removeLast();
+
+        assertThat(LLd.getLast()).isEqualTo(7);
+        assertThat(LLd.size()).isEqualTo(3);
+        assertThat(removeditem).isEqualTo(8);
+
+    }
+
+    @Test
+
+    // test remove first method 
+
+    public void testremovefirst(){
+        Deque61B<Integer> LLd = new LinkedListDeque61B<>();
+        LLd.addFirst(0);
+        LLd.addFirst(4);
+        LLd.addLast(7);
+        LLd.addLast(8);
+        int removeditem  = LLd.removeFirst();
+
+
+        assertThat(LLd.getFirst()).isEqualTo(0);
+        assertThat(LLd.size()).isEqualTo(3);
+        assertThat(removeditem).isEqualTo(4);
+
+    }
+
+    @Test 
+    // test get iterative 
+
+    public void testiterativeget(){
+        Deque61B<Integer> LLd = new LinkedListDeque61B<>();
+        LLd.addFirst(0);
+        LLd.addFirst(4);
+        LLd.addLast(7);
+        LLd.addLast(8);  
+        
+        assertThat(LLd.get(0)).isEqualTo(4);
+        assertThat(LLd.get(1)).isEqualTo(0);
+        assertThat(LLd.get(2)).isEqualTo(7);
+        assertThat(LLd.get(3)).isEqualTo(8);
+
+    }
+
+    @Test
+    public void testrecursiveget(){
+        Deque61B<Integer> LLd = new LinkedListDeque61B<>();
+        LLd.addFirst(0);
+        LLd.addFirst(4);
+        LLd.addLast(7);
+        LLd.addLast(8);  
+        
+        assertThat(LLd.getRecursive(0)).isEqualTo(4);
+        assertThat(LLd.getRecursive(1)).isEqualTo(0);
+        assertThat(LLd.getRecursive(2)).isEqualTo(7);
+        assertThat(LLd.getRecursive(3)).isEqualTo(8);
+
+    }
+}
